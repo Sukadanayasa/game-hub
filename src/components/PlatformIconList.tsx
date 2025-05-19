b479421daa9edd9deb4c2d53d5bafddf8f47895e
Sukadanayasa <kanangaya@gmail.com>
2025-05-19 16:13:00 +0800

    Build platform selector

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -4,13 +4,12 @@ import {
   FaXbox,
   FaApple,
   FaLinux,
-  FaAndroid,
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 interface Props {
   platforms: Platform[];
